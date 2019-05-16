542134dfb467213c427224ed2a09ca0c856aaf7b
Mati <mateusz.wygoda@gmail.com>
2019-05-16 21:12:58 +0200

    Add Setters and getters

--- src/Model/Unit.java
+++ src/Model/Unit.java
@@ -1,6 +1,22 @@
 package Model;
 
 public class Unit {
-    private int health;
+    private double health;
+    private double movementSpeed;
 
+    public void setHealth(double health) {
+        this.health = health;
+    }
+
+    public void setMovementSpeed(double movementSpeed) {
+        this.movementSpeed = movementSpeed;
+    }
+
+    public double getMovementSpeed() {
+        return movementSpeed;
+    }
+
+    public double getHealth() {
+        return health;
+    }
 }
